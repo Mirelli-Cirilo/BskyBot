24d11c0a4c6499257cafaef52850a484ac3f7995
Mirelli Cirilo <mirellicirilo44@gmail.com>
2024-09-13 23:52:08 -0300

    citações

--- src/boaNoite.ts
+++ src/boaNoite.ts
@@ -2,6 +2,7 @@ import { BskyAgent } from '@atproto/api';
 import * as dotenv from 'dotenv';
 import { CronJob } from 'cron';
 import * as process from 'process';
+import axios from 'axios';
 
 dotenv.config();
 
@@ -10,11 +11,23 @@ const agent = new BskyAgent({
     service: 'https://bsky.social',
   })
 
+async function getRandomQuote(): Promise<string>{
+    
+    try {
+        const response = await axios.get('https://api.quatable.com/random');
+        return response.data.quote; 
+    } catch (error) {
+        console.error('Erro ao obter citação:', error);
+        return 'Erro ao obter citação'; 
+    }
+}
 
 async function main() {
     await agent.login({ identifier: process.env.BLUESKY_USERNAME!, password: process.env.BLUESKY_PASSWORD!})
+
+    const quote = await getRandomQuote();
     await agent.post({
-        text: "🙂"
+        text: quote
     });
     console.log("Just posted!")
 }
@@ -24,8 +37,8 @@ main();
 
 // Run this on a cron job
 const scheduleExpressionMinute = '* * * * *'; // Run once every minute for testing
-const scheduleExpression = '0 */3 * * *'; // Run once every three hours in prod
+const scheduleExpression = '0 0 * * *'; // Run once every three hours in prod
 
-const job = new CronJob('0 */3 * * *', main); // change to scheduleExpressionMinute for testing
+const job = new CronJob(scheduleExpression, main); // change to scheduleExpressionMinute for testing
 
 job.start();
