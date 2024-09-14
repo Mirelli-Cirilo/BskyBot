de2ca1272e9d145012629a9399d88a0f369ec51e
Mirelli Cirilo <mirellicirilo44@gmail.com>
2024-09-13 22:32:41 -0300

    bskyexample

--- src/boaNoite.ts
+++ src/boaNoite.ts
@@ -2,32 +2,30 @@ import { BskyAgent } from '@atproto/api';
 import * as dotenv from 'dotenv';
 import { CronJob } from 'cron';
 import * as process from 'process';
-import { DateTime } from 'luxon';
 
 dotenv.config();
 
 // Create a Bluesky Agent 
 const agent = new BskyAgent({
     service: 'https://bsky.social',
-});
+  })
+
 
 async function main() {
-    await agent.login({ identifier: process.env.BLUESKY_USERNAME!, password: process.env.BLUESKY_PASSWORD! });
-
-    const dataAtual = DateTime.now().setZone('America/Sao_Paulo').toFormat("dd/MM/yyyy");
-
-    await agent.post({     
-        text: `Boa noite! A data de hoje é: ${dataAtual}`
+    await agent.login({ identifier: process.env.BLUESKY_USERNAME!, password: process.env.BLUESKY_PASSWORD!})
+    await agent.post({
+        text: "🙂"
     });
-    console.log("Just posted!");
+    console.log("Just posted!")
 }
 
 main();
 
-// Agendamento cron job para rodar uma vez por dia à 23h
 
-const scheduleExpression = '0 23 * * *'; // Executa todos os dias à 23h
+// Run this on a cron job
+const scheduleExpressionMinute = '* * * * *'; // Run once every minute for testing
+const scheduleExpression = '0 */3 * * *'; // Run once every three hours in prod
 
-const job = new CronJob(scheduleExpression, main);
+const job = new CronJob(scheduleExpression, main); // change to scheduleExpressionMinute for testing
 
 job.start();
